86f64b40bfca5e78394dd9722e521c2b0e692c19
Kirk Shoop <kirk.shoop@microsoft.com>
2015-03-30 10:54:44 -0700

    adds async_observable
    async_observable allows an expression to be constructed that is lazy.
    That is it can be stored and later when the results are needed it can be
    subscribed to multiple times.

---
+++ async_observable.h
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace async {
+
+    template<class T, class Done>
+    async_generator<T> when_done(async_generator<T> from, Done d) {
+        try {
+            for (auto& v : from) {
+                __yield_value v;
+            }
+        } catch(...) {
+            d();
+            throw;
+        }
+        d();
+    }
+
+    template<class T, class Subscribe = void>
+    class async_observable;
+
+    template<class T>
+    class async_observable<T, void>
+    {
+    public:
+        using value_type = T;
+
+        template<class Subscribe>
+        static auto create(Subscribe s) -> async_observable<T, Subscribe> {
+            return async_observable<T, Subscribe>{std::move(s)};
+        }
+
+        auto subscribe() const -> async_generator<T> {
+            // implementation of an empty sequence
+            __await ex::suspend_never();
+        }
+    };
+
+    template<class T, class Subscribe>
+    class async_observable
+    {
+        mutable Subscribe s;
+    public:
+        using value_type = T;
+
+        async_observable(Subscribe s) : s(std::move(s)) {}
+
+        auto subscribe() const -> async_generator<T> {
+            return s();
+        }
+
+        template<class Lifter,
+            class SG = std::result_of_t<Subscribe()>,
+            class LG = std::result_of_t<Lifter(SG)>,
+            class U = typename LG::value_type>
+        auto lift(Lifter l) const {
+            auto copy = s;
+            return async_observable<U>::create(
+                [=]() mutable -> async_generator<U> {
+                    return l(copy());
+                });
+        }
+    };
+}
